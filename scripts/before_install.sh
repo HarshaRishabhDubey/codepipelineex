cf7de2116153c01e68f7bee9661e206d21ba9588
Harsh Dubey <40870351+HarshaRishabhDubey@users.noreply.github.com>
2024-04-22 00:06:20 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,11 +3,8 @@
 #_Change_Working_Directory
 cd /home/ec2-user/server
 
-#_Update_&_Set_Node_Version
-curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
-
 #_Download_Node_&NPM
-sudo yum -y install nodejs npm
+sudo yum -y install nodejs
 
 #_Download_PM2
 sudo npm install pm2@latest -g
